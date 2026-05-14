47c4ce5f3f94a05ce9619db52a9c9bb46b153ca3
Afzal Khan <afzalkhan01765@gmail.com>
2026-05-14 07:59:52 +0600

    remove kth possition linklist

--- insert_data_end.cpp
+++ insert_data_end.cpp
@@ -12,28 +12,22 @@ public:
     }
 };
 
-// Insert at end
+
 Node* insert_data_end(Node* head, int x){
     Node* new_node = new Node(x);
 
-    // If list is empty
     if(head == nullptr){
         return new_node;
     }
-
-    // Traverse to last node
     
     Node* temp = head;
     while(temp->next != nullptr){
         temp = temp->next;
     }
-
     temp->next = new_node;
-
     return head;
 }
 
-// Print list
 void print_list(Node* head){
     while(head != nullptr){
         cout << head->data << endl;
@@ -41,13 +35,38 @@ void print_list(Node* head){
     }
 }
 
+Node * remove_node(Node * head , int pos){
+
+    cout<<"EEEEEEEEEEE", pos <<endl;
+    if(pos <=1){
+        return head;
+    }
+    int cnt = 1;
+    Node * temp = head;
+
+    while(temp != nullptr){
+        if(cnt == pos){
+            temp->next = temp ->next->next;
+        }
+        temp = temp - >next;
+        cnt++
+    }
+
+    return temp;
+}
+
 int main(){
+
     Node* head = new Node(1);
     head->next = new Node(2);
     head->next->next = new Node(3);
     head->next->next->next = new Node(4);
+    head ->next->next->next->next = new Node(5);
+    head ->next->next->next->next->next = new Node(6);
 
     head = insert_data_end(head, 33434);
+
+    remove_node(head,2);
 
     print_list(head);
 
