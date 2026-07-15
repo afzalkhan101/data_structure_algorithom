e219ad7a9c2c91ba01c8c0f4cd35d566ace917de
Afzal Khan <afzalkhan01765@gmail.com>
2026-07-15 07:24:41 +0600

    updated code

--- linklist_node.cpp
+++ linklist_node.cpp
@@ -4,7 +4,8 @@ using namespace std;
 class Node{
     public : 
        int data;
-       Node * next ;
+       Node * next ; 
+       
        Node(int value){
           this ->data = value;
           this ->next = nullptr;
