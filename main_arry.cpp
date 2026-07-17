0c8c35bc03f434d0454bdb26e0c1597623cd0cec
Afzal Khan <afzalkhan01765@gmail.com>
2026-07-17 18:27:58 +0600

    fixed and updated code

---
+++ main_arry.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print_array(){
+    int arr[5] = {1 ,2 ,3 ,4 , 5 };
+    for(int i =  0; i<5 ;  i++){
+        cout<<arr[i]<<endl;
+    }
+}
+
+int main(){
+
+    print_array();
+
+
+    return 0;
+}
